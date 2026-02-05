8e6a1de808e60a8d6b6ded2a23c137c5f607369b
Jcasillas11 <jcasillas.menendez@gmail.com>
2026-02-05 10:12:26 +0100

    Commit inscripcionimpl

--- src/main/java/es/fplumara/dam1/talleres/service/impl/InscripcionServiceImpl.java
+++ src/main/java/es/fplumara/dam1/talleres/service/impl/InscripcionServiceImpl.java
@@ -1,5 +1,75 @@
 package es.fplumara.dam1.talleres.service.impl;
 
-public class InscripcionServiceImpl {
+import es.fplumara.dam1.talleres.exception.DatosIncripcionUsuarioServiceException;
+import es.fplumara.dam1.talleres.exception.DatosInscripcionTallerServiceException;
+import es.fplumara.dam1.talleres.model.Inscripcion;
+import es.fplumara.dam1.talleres.model.Taller;
+import es.fplumara.dam1.talleres.model.Usuario;
+import es.fplumara.dam1.talleres.repository.InscripcionRepository;
+import es.fplumara.dam1.talleres.repository.TallerRepository;
+import es.fplumara.dam1.talleres.repository.UserRepository;
+import es.fplumara.dam1.talleres.service.InscripcionService;
 
+import java.util.List;
+
+public class InscripcionServiceImpl implements InscripcionService {
+
+     private InscripcionRepository InscripcionRepository;
+     private TallerRepository TallerRepository;
+     private UserRepository UserRepository;
+
+
+
+
+
+
+
+
+
+    @Override
+    public Inscripcion inscribirUsuario(Long tallerid, Long usuarioid, String rol) {
+        //1. Comprobar si el taller existe o no
+        Taller Taller = TallerRepository.findById(tallerid);
+        if (Taller == null) {
+            throw new DatosInscripcionTallerServiceException("El taller no existe, no se encuentra");
+        }
+        return null;
+
+        //2. Comprobar si el usuario existe
+        Usuario usuario = UserRepository.findById(usuarioid);
+        if (usuario == null) {
+            throw new DatosIncripcionUsuarioServiceException("El usuario no existe, no se encuentra");
+        }
+    }
+
+
+    @Override
+    public Inscripcion cambiarRol(Long tallerid, Long usuarioid, String nuevoRol) {
+        return null;
+    }
+
+    @Override
+    public Inscripcion expulsarUsuario(Long tallerid, Long usuarioid) {
+        return null;
+    }
+
+    @Override
+    public List<Inscripcion> listarInscripcionesDETaller(Long tallerid) {
+        return List.of();
+    }
+
+    @Override
+    public List<Inscripcion> listarInscripcionesDeUsuario(Long Usuarioid) {
+        return List.of();
+    }
+
+    @Override
+    public Inscripcion verMiembrosAgrupadosPorPerfil(Long tallerid) {
+        return null;
+    }
+
+    @Override
+    public Inscripcion verResponsables(Long tallerid) {
+        return null;
+    }
 }
